465f3f4a653581f50044f54cffc9ac57cb4d7855
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-06-09 20:09:24 -0500

    isomorphic fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    isLoading: true,
+    contacts: []
+  };
 }
 
+componentDidMount(){
+  this.fetchData();
+}
+
+fetchData = () => {
+fetch('https://randomuser.me/api/?results=10')
+.then( response => response.json())
+.then(data => data.results.map(user => ({
+  name: `${user.name.first}`,
+  address:`${user.location.street}, ${user.location.city}`,
+  email: `${user.email}`
+}
+
+)))
+.then(contacts => this.setState({
+  contacts,
+  isLoading: false
+}))
+
+
+
+.catch(error => console.log("parsing failed...", error))
+}
+
+  render(){
+    // const{isLoading, contacts} = this.state;
+  return (
+      <div className="App">
+        <div>   
+       {
+            !this.state.isLoading && this.state.contacts.length > 0 ? this.state.contacts.map(contact => {
+             return <p key={contact.name}><br/>{contact.name}<br/>{contact.address}<br/>{contact.email}</p>
+            }) : null
+
+          }
+        </div>
+        </div>
+
+  )}
+        }
 export default App;
